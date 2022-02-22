cadcedc4f073354627feb1f3653aee192825f60a
Julian Len <julianlen@gmail.com>
2022-02-22 15:41:07 -0300

    WrapperMutableRepository is created for each transaction, and it keeps a bucket manager that tracks the read and written keys, when commit is called it stores all the r/w keys in the bucket manager.

--- rskj-core/src/main/java/co/rsk/core/bc/BlockExecutor.java
+++ rskj-core/src/main/java/co/rsk/core/bc/BlockExecutor.java
@@ -31,6 +31,7 @@ import com.google.common.annotations.VisibleForTesting;
 import org.ethereum.config.blockchain.upgrades.ActivationConfig;
 import org.ethereum.config.blockchain.upgrades.ConsensusRule;
 import org.ethereum.core.*;
+import org.ethereum.db.WrapperMutableRepository;
 import org.ethereum.vm.DataWord;
 import org.ethereum.vm.PrecompiledContracts;
 import org.ethereum.vm.program.ProgramResult;
@@ -456,7 +457,8 @@ public class BlockExecutor {
         // the state prior execution again.
         Metric metric = profiler.start(Profiler.PROFILING_TYPE.BLOCK_EXECUTE);
 
-        Repository track = repositoryLocator.startTrackingAt(parent); //TODO(JULI): Devuelve un MutableTrie(MutableTrieCache(Trie snapshot at parent))
+        Repository track = repositoryLocator.startTrackingAt(parent);
+        ReadWrittenKeysTracker readWrittenKeysTracker = new ReadWrittenKeysTracker();
 
         maintainPrecompiledContractStorageRoots(track, activationConfig.forBlock(block.getNumber()));
 
@@ -466,6 +468,7 @@ public class BlockExecutor {
         List<TransactionReceipt> receipts = new ArrayList<>();
         List<Transaction> executedTransactions = new ArrayList<>();
         Set<DataWord> deletedAccounts = new HashSet<>();
+        WrapperMutableRepository wrapperMutableRepository = new WrapperMutableRepository(track, readWrittenKeysTracker);
 
         int txindex = 0;
 
@@ -476,7 +479,7 @@ public class BlockExecutor {
                     tx,
                     txindex++,
                     block.getCoinbase(),
-                    track,
+                    wrapperMutableRepository,
                     block,
                     totalGasUsed,
                     vmTrace,
@@ -497,6 +500,7 @@ public class BlockExecutor {
             }
 
             executedTransactions.add(tx);
+            readWrittenKeysTracker.clear();
 
             if (this.registerProgramResults) {
                 this.transactionResults.put(tx.getHash(), txExecutor.getResult());
